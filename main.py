8d967bb2a526dd39fac2482446eb1074fc6895fc
ShashiSable <shsable84@gmail.com>
2023-01-01 10:11:20 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+a=int(input('A'))
+b=int(input('B'))
+if a>=b:
+    if a==b:
+        print("a is equal to b")
+    else:
+        print("a is greater than b")
+else:
+    print("a is less than b")
+
+
+
